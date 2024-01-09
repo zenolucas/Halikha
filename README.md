6cd23406693fbac4ce29e6397e5979d4f2634532
Zen Lucas <2200501@slu.edu.ph>
2024-01-09 15:50:28 +0800

    bootstrap template + go backend serving index.html

--- README.md
+++ README.md
@@ -1,13 +1,7 @@
 # Halikha
 attempt at a startup idea
+will practice w/ bootstrap templates and some go backend stuff using the Fiber framework.
 
-will be using Go lang for backend, 
-maybe I could already start with login/logout.
+Todo:
 
-
-
-AND AIR / LIVE RELOAD IS NOT FUCKING WORKING!!! PUTANG INA TALAGA NAKAKAINIS POTA.
-OKAY NA! I'VE WRESTLED WITHT HIS SHIT FOR A BIT, I'M DONE FOR THE NIGHT.
-
-
-for today, I tried shopify, just to get my head around ecommerce sites.
+basic login, hence try to play around w/ database already.
--- server.go
+++ server.go
@@ -1,21 +1,29 @@
 package main
 
 import (
-    "log"
+	"log"
 
-    "github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
+	// Serve Bootstrap CSS file
+	app.Static("/bootstrap/css", "./node_modules/bootstrap/dist/css")
+
+	// Serve Bootstrap JS files
+	app.Static("/bootstrap/js", "./node_modules/bootstrap/dist/js")
+	app.Static("/bootstrap/js/popper", "./node_modules/@popperjs/core/dist/umd")
+
+	// Serve other static files
 	app.Static("/", "./public")
 
-    app.Get("/", login)
+	app.Get("/", login)
 
-    log.Fatal(app.Listen(":3000"))
+	log.Fatal(app.Listen(":3000"))
 }
 
 func login(c *fiber.Ctx) error {
-	return c.SendFile("public/login.html")
+	return c.SendFile("public/index.html")
 }
